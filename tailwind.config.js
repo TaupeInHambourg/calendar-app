586d4676d6608353936a05b5fb7ab8e3a057dd87
Audrey Norvez <audrey.norvez@gmail.com>
2025-01-09 15:31:03 +0100

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    colors: {
+      'fjord': {
+        '50': '#f5f7fa',
+        '100': '#eaeef4',
+        '200': '#d0dae7',
+        '300': '#a8bbd1',
+        '400': '#7896b8',
+        '500': '#5779a0',
+        '600': '#3e5879',
+        '700': '#384f6c',
+        '800': '#31435b',
+        '900': '#2d3a4d',
+        '950': '#1e2633',
+      },
+      'bone': {
+        '50': '#f9f5f3',
+        '100': '#f1e9e3',
+        '200': '#d8c4b6',
+        '300': '#ceb4a3',
+        '400': '#b9927e',
+        '500': '#ab7864',
+        '600': '#9d6859',
+        '700': '#83544b',
+        '800': '#6b4641',
+        '900': '#573b37',
+        '950': '#2e1e1c',
+      },
+      'rainbow': {
+        'melon': '#ffadad',
+        'sunset': '#ffd6a5',
+        'cream': '#fdffb6',
+        'tea-green': '#caffbf',
+        'electric-blue': '#9bf6ff',
+        'jordy-blue': '#a0c4ff',
+        'periwinkle': '#bdb2ff',
+        'lavender': '#ffc6ff',
+        'baby-powder': '#fffffc',
+      }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  plugins: [],
 }
-
